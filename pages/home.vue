bd5247101a25b34bff57cf7cf10b6df3744a3bf8
OscarCanongo <a01730443@itesm.mx>
2019-03-30 01:42:09 -0600

    a

---
+++ pages/home.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div>
+            <img src="@/static/logo.png" width="20" height="20">
+        </div>
+        <div class="container">
+            <div class="row">
+            <div class="col-sm">
+            <img src="../static/route1.png">
+        </div>
+        <div class ="col-sm">
+            <img src="../static/star.png">
+            </div>
+        
+
+
+        <div class="col-sm">
+            <img src="@/static/rayas.svg" width="20" height="20">
+        </div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.letrabonita {
+  font-size: 22px;
+  font: bold;
+}
+.sombra {
+  box-shadow: 0 2px 6px rgba(39, 39, 39, 0.13), 0 2px 6px rgba(39, 39, 39, 0.13);
+}
+</style>
