0ab21cfa9cd35b8d27b9ab0a59b7c85709760c39
Abigail González Hidalgo <abigailgonzalezh@gmail.com>
2019-03-30 11:28:37 -0600

    login

--- pages/sesion.vue
+++ pages/sesion.vue
@@ -4,18 +4,20 @@
         <div class="col-10">
                 <br>
                 <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
+            <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-5">
+                    <img src="../static/logo.png" width="350">
+                </div>
+            </div>
+        </div>
+        <br>
+                <br>
+                <br>
+                <br>
             <div>
            <div> <label for="name">Email</label></div>
-            <input type="text" class="form-control col-md-4" id="name">
+            <input type="email" class="form-control col-md-4" id="name">
             </div>
             <br>
             
@@ -23,6 +25,8 @@
                <div> <label for="name" >Contraseña</label></div>
                 <input type="password" class="form-control col-md-4" id="name"> 
                 </div> 
+                <br>
+                <br>
                 <br>
                 <a href="home">
                 <button type="submit" class="btn btn-dark">
@@ -53,21 +57,39 @@ export default {
   box-shadow: 0 2px 6px rgba(39, 39, 39, 0.13), 0 2px 6px rgba(39, 39, 39, 0.13);
 }
 .second {
- background-image: url("../static/bg3.png");
+ background-image: url("../static/bg7.png");
 
  background-position: 65% 10%;
  background-color: #ffffff;
- background-size: auto;
+ background-size: 1100px;
  }
 
  .col-10{
  text-align: center;
  }
 
-
+.btn-dark {
+  background-color: black;
+}
 
  input[type=text] {
   border: 2px solid black;
   border-radius: 4px;
+}
+
+ input[type=email] {
+  border: 2px solid black;
+  border-radius: 4px;
+  align-content: center;
+}
+
+ input[type=password] {
+  border: 2px solid black;
+  border-radius: 4px;
+  align-content: center;
+}
+
+.label{
+  size: 20px;
 }
 </style>
