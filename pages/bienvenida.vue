f8f3b0a317d23533a01afc27805be5caf01cc5e2
OscarCanongo <a01730443@itesm.mx>
2019-03-30 05:08:16 -0600

    sdaasd

---
+++ pages/bienvenida.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="container second">
+        
+        <div class="col-10">
+                <br>
+                <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+
+           <div> <label for="name">BIENVENIDO NOMBRE</label></div>
+         
+        
+            <br>
+                <div>
+                <a href="">
+                <button type="submit" class="btn btn-dark">
+                    <div>Modifica contraseña</div>
+                </button>
+                </a>
+                </div>
+                <br>
+    
+                <div>
+                <a href="">
+                <button type="submit" class="btn btn-dark">
+                    <div>Modifica e-mail</div>
+                </button>
+                </a>
+                </div>
+                <br>
+
+                <div>
+                <a href="">
+                <button type="submit" class="btn btn-dark">
+                    <div>Modificar nombre</div>
+                </button>
+                </a>
+                </div>
+        </div> 
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+.form-control{
+ display: inline-block;
+ text-align: center;
+}
+.letrabonita {
+  font-size: 22px;
+  font: bold;
+}
+.sombra {
+  box-shadow: 0 2px 6px rgba(39, 39, 39, 0.13), 0 2px 6px rgba(39, 39, 39, 0.13);
+}
+.second {
+ background-image: url("../static/bg2.png");
+
+ background-position: 65% 10%;
+ background-color: #ffffff;
+ background-size: auto;
+ }
+
+ .col-10{
+ text-align: center;
+ }
+
+
+
+ input[type=text] {
+  border: 2px solid black;
+  border-radius: 4px;
+}
+</style>
