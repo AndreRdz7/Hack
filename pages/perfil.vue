a44e0f1704447b621df9427990e4a228d673a20c
unknown <ulises_23cg@hotmail.com>
2019-03-30 16:34:25 -0600

    iuhkj

---
+++ pages/perfil.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="container second">
+            <div class="row align-items-start">
+           <a href="home"><img src="../static/return.png" width="100" height="100"></a>
+            </div>
+
+            <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-6">
+                    <img src="../static/logo.png" width="350" height="">
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-10">
+
+
+                <br>
+
+                         <div><h1 align="center"><b>BIENVENIDO NOMBRE</b></h1></div>
+                <br>
+            <br>
+ 
+            
+           
+                <div>
+                <a href="">
+                <button type="submit" class="btn btn-dark btn-lg btn-block">
+                    <div><h4>Modificar contraseña</h4></div>
+                </button>
+                </a>
+                </div>
+                <br>
+    
+                <div>
+                <a href="">
+                <button type="submit" class="btn btn-dark btn-lg btn-block">
+                    <div><h4>Modificar e-mail</h4></div>
+                </button>
+                </a>
+                </div>
+                <br>
+
+                <div>
+                <a href="">
+                 <button type="submit" class="btn btn-dark btn-lg btn-block">
+                    <div><h4>Modificar nombre</h4></div>
+                </button>
+                </a>
+                <br>
+             <div>
+                 
+                
+                <a href="">
+                 <button type="submit" class="btn btn-dark btn-lg btn-block">
+                    <div><h4>Modificar tarjeta</h4></div>
+                </button>
+                </a>
+                <br>
+                
+                <div>
+                <a href="">
+                <button type="submit" class="btn btn-dark btn-lg btn-block">
+                    <div><h4>Cerrar sesion</h4></div>
+                </button>
+                </a>
+                <br>
+                
+                </div>
+                </div>
+                </div>
+        </div> 
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+.form-control{
+ display: inline-block;
+ text-align: center;
+}
+.letrabonita {
+  font-size: 22px;
+  font: bold;
+}
+.sombra {
+  box-shadow: 0 2px 6px rgba(39, 39, 39, 0.13), 0 2px 6px rgba(39, 39, 39, 0.13);
+}
+.second {
+ background-image: url("../static/bg2.png");
+
+ background-position: 65% 10%;
+ background-color: #ffffff;
+ background-size: auto;
+ }
+
+ .col-10{
+ text-align: center;
+ }
+
+
+
+ input[type=text] {
+  border: 2px solid black;
+  border-radius: 4px;
+}
+</style>
