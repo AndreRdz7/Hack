3e8b7174bb9568189809837952edfa0d6a918a81
Abigail González Hidalgo <abigailgonzalezh@gmail.com>
2019-03-30 07:07:56 -0600

    home v1

--- pages/home.vue
+++ pages/home.vue
@@ -5,7 +5,7 @@
         </div>-->
 
         <div class="container">
-            <div class="row justify-content-md-center">
+            <div class="row justify-content-md-left">
                 <div class="col-6 col-md-4">
                     <img src="../static/logo.png" width="250" height="150">
                 </div>
@@ -13,11 +13,36 @@
         </div>
 
         <br>
-        
+        <br>
+        <br>
+
         <div class="container">
             <div class="row">
-                <div class="col">
-                    <button type="button" class="btn btn-light"><img src="../static/Rutas_button.svg" width="130" height="130"></button>
+                <div class="col-6 col-md-4">
+                    <button type="button" class="btn btn-light"><img src="../static/Rutas_button.svg" width="230" height="230"></button>
+                </div>
+                <div class="col-6 col-md-4">
+                    <button type="button" class="btn btn-light"><img src="../static/Favoritos_button.svg" width="230" height="230"></button>
+                </div>
+                <div class="col-6 col-md-4">
+                    <button type="button" class="btn btn-light"><img src="../static/Huella_Verde_button.svg" width="330" height="230"></button>
+                </div>
+            </div>
+        </div>
+
+        <br>
+        <br>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-6 col-md-4">
+                    <button type="button" class="btn btn-light"><img src="../static/Incidentes_button.svg" width="230" height="230"></button>
+                </div>
+                <div class="col-6 col-md-4">
+                    <button type="button" class="btn btn-light"><img src="../static/Perfil_button.svg" width="230" height="230"></button>
+                </div>
+                <div class="col-6 col-md-4">
+                    <button type="button" class="btn btn-light"><img src="../static/Historial_button.svg" width="330" height="220"></button>
                 </div>
             </div>
         </div>
@@ -41,5 +66,9 @@ export default {
 
 .light{
     background-color: white;
+}
+
+.body { 
+    margin: 0; 
 }
 </style>
