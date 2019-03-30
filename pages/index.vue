29887bddea84eaed01ffcdf01f5551687c7326ec
OscarCanongo <a01730443@itesm.mx>
2019-03-30 03:17:10 -0600

    a

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,11 @@
   <section class="container body">
     <div>
         <div class="sesion">
-          <button type="button" class="btn btn-dark">Iniciar Sesion</button>
+          <button type="button" class="btn btn-dark btn-lg">Iniciar Sesion</button>
         </div>
          <div class="registro" >
             <!-- <nuxt-link :to="{ name: 'register'}" replace> -->
-          <a href="register"><button type="button" class="btn btn-dark" >Registrarse</button></a>
+          <a href="register"><button type="button" class="btn btn-dark btn-lg" >Registrarse</button></a>
           <!-- </nuxt-link> -->
         </div>
       </div>
