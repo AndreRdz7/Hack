5c57806438083153a7846eb389880718780a579f
OscarCanongo <a01730443@itesm.mx>
2019-03-30 05:22:41 -0600

    sasd

---
+++ pages/huella.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="container second">
+        
+        <div class="col-10">
+                <br>
+                <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+
+    
+          <div> <label for="name">TU HUELLA VERDE</label></div>
+          <div> <label for="name">ULISES haz conseguido estas recompensas: </label></div>
+        
+            <br>
+           
+                <div>
+                <a href="">
+                <button type="submit" class="btn btn-danger btn-lg btn-block">
+                    <div>Dato 1</div>
+                </button>
+                </a>
+                </div>
+                <br>
+    
+                <div>
+                <a href="">
+                <button type="submit" class="btn btn-danger btn-lg btn-block">
+                    <div>Dato 2</div>
+                </button>
+                </a>
+                </div>
+                <br>
+
+                <div>
+                <a href="">
+                 <button type="submit" class="btn btn-danger btn-lg btn-block">
+                    <div>Dato 3</div>
+                </button>
+                </a>
+                <br>
+                <br>
+                <a href="home">
+                <button type="submit" class="btn btn-dark">
+                    <div>Conocer recompensas</div>
+                </button>
+                </a>
+                </div>
+        </div> 
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+.form-control{
+ display: inline-block;
+ text-align: center;
+}
+.letrabonita {
+  font-size: 22px;
+  font: bold;
+}
+.sombra {
+  box-shadow: 0 2px 6px rgba(39, 39, 39, 0.13), 0 2px 6px rgba(39, 39, 39, 0.13);
+}
+.second {
+ background-image: url("../static/bg2.png");
+
+ background-position: 65% 10%;
+ background-color: #ffffff;
+ background-size: auto;
+ }
+
+ .col-10{
+ text-align: center;
+ }
+
+
+
+ input[type=text] {
+  border: 2px solid black;
+  border-radius: 4px;
+}
+</style>
