ffb4e7ce8cd31e200d417d75367d207686b2c7b9
OscarCanongo <a01730443@itesm.mx>
2019-03-30 09:14:27 -0600

    a

---
+++ pages/favoritos.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="container second">
+
+        
+        <div class="col-10">
+
+            <div class="row align-items-start">
+           <a href="rutas"><img src="../static/return.png" width="100" height="100"></a>
+            </div>
+                <br>
+                <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            
+           
+                <div>
+                <a href="">
+                <button type="submit" class="btn btn-danger btn-lg btn-block">
+                    <div><h4>Ruta Casa</h4></div>
+                </button>
+                </a>
+                </div>
+                <br>
+    
+                <div>
+                <a href="">
+                <button type="submit" class="btn btn-danger btn-lg btn-block">
+                    <div><h4>Ruta Escuela</h4></div>
+                </button>
+                </a>
+                </div>
+                <br>
+
+                <div>
+                <a href="">
+                 <button type="submit" class="btn btn-danger btn-lg btn-block">
+                    <div><h4>Casa Novio</h4></div>
+                </button>
+                </a>
+                <br>
+                <br>
+                <a href="home">
+
+                </a>
+                </div>
+        </div> 
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+.form-control{
+ display: inline-block;
+ text-align: center;
+}
+.letrabonita {
+  font-size: 22px;
+  font: bold;
+}
+.sombra {
+  box-shadow: 0 2px 6px rgba(39, 39, 39, 0.13), 0 2px 6px rgba(39, 39, 39, 0.13);
+}
+.second {
+ background-image: url("../static/bg2.png");
+
+ background-position: 65% 10%;
+ background-color: #ffffff;
+ background-size: auto;
+ }
+
+ .col-10{
+ text-align: center;
+ }
+
+
+
+ input[type=text] {
+  border: 2px solid black;
+  border-radius: 4px;
+}
+</style>
